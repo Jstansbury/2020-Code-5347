2c7128b0138b1aaf4a2f97b5efcb346eafb9806d
jstansbury <jstansbury2020@csw.org>
2020-02-17 12:11:06 -0500

    fixed error

--- src/main/java/frc/robot/commands/TogglePneu.java
+++ src/main/java/frc/robot/commands/TogglePneu.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.BeaverLift;
-import frc.robot.subsystems.PneumticSUB;
+
 
 public class TogglePneu extends CommandBase {
   /**
